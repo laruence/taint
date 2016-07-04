9bc21bc18c12089dd4787cb39cc0901de180859b
Xinchen Hui <laruence@gmail.com>
2016-07-04 12:15:49 +0800

    prepare for 2.0.2

--- php_taint.h
+++ php_taint.h
@@ -32,7 +32,7 @@ extern zend_module_entry taint_module_entry;
 #include "TSRM.h"
 #endif
 
-#define PHP_TAINT_VERSION "2.0.2-dev"
+#define PHP_TAINT_VERSION "2.0.2"
 
 /* it's important that make sure 
  * this value is not used by Zend or
